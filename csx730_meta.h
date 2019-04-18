5a961856126b5188aae70e189f5ef42dc6e700f3
Michael Cotterell (Supa' Mike) <mepcotterell@gmail.com>
2019-04-17 20:54:36 -0400

    added meta-data api

---
+++ csx730_meta.h
@@ -0,0 +1,24 @@
+#ifndef CSX730_META_H
+#define CSX730_META_H
+
+#include <time.h>
+#include <semaphore.h>
+#include "csx730_data.h"
+
+/**
+ * Structure for an inode.
+ */
+typedef struct inode {
+  char           name [255]; /**< Name of the file. */
+  unsigned long  ino;        /**< Inode number. */
+  bool           dir;        /**< Whether or not the file is a directory. */
+  size_t         size;       /**< Size of the file. */
+  size_t         bno;        /**< Starting block number for file data. */
+  size_t         offset;     /**< Offset of file data in starting block. */
+  time_t         atime;      /**< Time of last file access. */
+  struct inode * prev;       /**< Previous inode. */
+  struct inode * next;       /**< Next inode. */
+  sem_t          sem;        /**< Semaphore. */
+} inode_t;
+
+#endif // CSX730_META_H
