587fe51232e3163e9c0097db32857748598d2161
dsluo <mail@dsluo.me>
2019-04-28 22:35:26 -0400

    preliminary impl of vfs_init

--- csx730_vfs.c
+++ csx730_vfs.c
@@ -1,6 +1,7 @@
 #include "csx730_vfs.h"
 #include "csx730_ioctl.h"
 #include <string.h>
+#include <semaphore.h>
 
 #define SUCCESS(call) if (!(call)) return false
 #define VOID_MATH(math_stuff) (void *)((char *) math_stuff)
@@ -63,26 +64,34 @@ bool disk_write(disk_t * disk, size_t offset, size_t len, void * data) {
 }
 
 bool csx730_vfs_init(const char * disk_image, size_t size) {
-    disk_t disk;
 
-    SUCCESS(csx730_ioctl_open(&disk, disk_image, size));
+    // TODO: calculate space to reserve for inodes + superblock here?
 
-    inode_t root = {
-        .name={'/'},
-        .ino=1,
-        .dir=true,
-        .size=0,
-        .bno=0,
-        .offset=sizeof(superblock_t),
-        .atime=0, // todo
-        .prev=0,
-        .next=0,
-        .child=0,
-        // .sem={ 0 } // todo
-    };
+    SUCCESS(csx730_ioctl_open(&__global.disk, disk_image, size));
 
     superblock_t superblock = {
         .magic=0xDEADBEEF,
         .root=1
     };
+
+    SUCCESS(disk_write(&__global.disk, 0, sizeof(superblock_t), (void *) &superblock));
+
+    inode_t root = {
+        .name={'/'},
+        .ino=1,
+        .dir=true,
+        .size=0, // todo: what should this be for directories
+        .bno=0,
+        .offset=sizeof(superblock_t),
+        .atime=0, // todo time of creation
+        .prev=0,
+        .next=0,
+        .child=0,
+    };
+    // todo: idk how much this should be initialized for.
+    SUCCESS(sem_init(&root.sem, true, 1));
+
+    SUCCESS(disk_write(&__global.disk, sizeof(superblock_t), sizeof(inode_t), (void *) &root));
+
+    return true;
 }
