da9c77f010f00c2d7fc2e82229f014f9f1930ec9
dsluo <mail@dsluo.me>
2019-04-30 13:46:28 -0400

    utility functions

--- csx730_extra.c
+++ csx730_extra.c
@@ -47,3 +47,81 @@ bool disk_write(disk_t * disk, size_t offset, size_t len, void * data) {
     
     return true;
 }
+
+inode_t * get_inode(const char ** path, inode_t table[]) {
+    inode_t * curr = table;
+
+    if (!strcmp(path[0], "/") && path[1] == NULL) {
+        return curr;
+    }
+
+    const char * filename = path[0];
+    while (curr->dir && filename != NULL) {
+        if (curr->child == NULL_INODE)
+            return NULL;
+
+        inode_t * next = &table[curr->child - 1];
+        while (strcmp(next->name, filename)) {
+            if (next->next == NULL_INODE)
+                return NULL;
+            next = &table[next->next - 1];
+        }
+        curr = next;
+        filename++;
+    }
+
+    return !strcmp(curr->name, filename) ? curr : NULL;
+}
+
+inode_t * allocate_inode(inode_t table[], size_t table_size) {
+    for (size_t i = 0; i < table_size; i++)
+        if (table[i].ino == NULL_INODE) {
+            table[i].ino = i + 1;
+            return &table[i];
+        }
+    return NULL;
+}
+
+void deallocate_inode(inode_t * inode);
+
+const char ** dirname(const char * path[]) {
+    size_t pathlen = 0;
+    for (const char ** i = path; *i != NULL; i++)
+        pathlen++;
+    
+    if (pathlen == 1) {
+        char ** parent = malloc(2 * sizeof(char *));
+        parent[0] = malloc(sizeof(const char *));
+        const char * root = "/";
+        strcpy(parent[0], root);
+        parent[1] = NULL;
+        return (const char **) parent;
+    }
+
+    const char ** parent = calloc(pathlen, sizeof(const char *));
+    for (const char ** i = path, ** j = parent; *i != NULL; i++, j++) {
+        *j = malloc(strlen(*i));
+        memcpy((void*) *j, *i, strlen(*i));
+    }
+    parent[pathlen] = NULL;
+
+    return parent;
+}
+
+void free_dirname(const char ** path) {
+    size_t pathlen = 0;
+    for (const char ** i = path; *i != NULL; i++)
+        pathlen++;
+    
+    for (const char ** i = path; *i != NULL; i++)
+        free((void *) *i);
+    free(path);
+}
+
+void basename(const char ** path, const char name[]) {
+    size_t pathlen = 0;
+    for (const char ** i = path; *i != NULL; i++)
+        pathlen++;
+    
+    strcpy((char *) name, path[pathlen - 1]);
+}
--- csx730_extra.h
+++ csx730_extra.h
@@ -40,4 +40,10 @@ bool disk_write(disk_t * disk, size_t offset, size_t len, void * data);
  */
 bool disk_read(disk_t * disk, size_t offset, size_t len, void * data);
 
+// todo: there should probably be a const somewhere in this header
+inode_t * get_inode(const char ** path, inode_t * table);
+inode_t * allocate_inode(inode_t * table, size_t table_size);
+const char ** dirname(const char ** path);
+void free_dirname(const char ** path);
+void basename(const char **path, const char name[]);
 #endif
