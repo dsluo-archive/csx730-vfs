faa08fb0fafdefc94aed3d44fcba93125064da84
dsluo <mail@dsluo.me>
2019-04-29 22:16:58 -0400

    read/write test script

--- csx730_vfs_driver.c
+++ csx730_vfs_driver.c
@@ -4,22 +4,38 @@
 
 #include <string.h>
 #include <stdio.h>
+#include "csx730_extra.h"
 
 int main() {
 
   // your test code here
 
   disk_t disk;
-  const char path[4096] = "/home/dsluo/vdisk.img";
-  size_t size = 20;
+  const char * path = "/home/dsluo/csx730-vfs/vdisk.img";
+  size_t size = 3;
+
   csx730_ioctl_open(&disk, path, size);
 
-  // disk_write(&disk, 0, strlen(path), (void *) path);
-  csx730_ioctl_put(&disk, 0, (void *) &path);
+  char data[8192];
+  char * numbers = "0123456789";
 
-  char buffer[4096];
-  csx730_ioctl_get(&disk, 0, (void *) buffer);
-  printf(buffer);
+  for (int i = 0; i < 8192; i++) {
+    data[i] = numbers[i%10];
+  }
+
+  disk_write(&disk, 2048, 8192, data);
+
+  memset(data, '\0', 8192);
+
+  disk_read(&disk, 2048, 8192, data);
+
+  // csx730_vfs_init(path, size);
+
+  // inode_t inode;
+
+  // const char * path2 = "/";
+  // csx730_stat(&path2, &inode);
+
   return 0;
 
 } // main
