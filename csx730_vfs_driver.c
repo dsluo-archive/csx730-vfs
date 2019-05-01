6403470e3d877fd4dc5e0a582f2d1ea2ad3b4b36
dsluo <mail@dsluo.me>
2019-04-30 22:32:16 -0400

    test driver

--- csx730_vfs_driver.c
+++ csx730_vfs_driver.c
@@ -15,22 +15,28 @@ int main()
     // size_t size = 20;
 
     // initialize the file system
-    csx730_vfs_init("vdisk.img", 256);
+    csx730_vfs_init("vdisk.img", 20);
 
     // setup paths for  /home/root/README.md
     const char *home[] = {"home", NULL};
     const char *home_root[] = {"home", "root", NULL};
     const char *home_root_readme[] = {"home", "root", "README.md", NULL};
+    const char *home_root_readme2[] = {"home", "root", "README2.md", NULL};
 
     // create directories and files
     csx730_creat(home, true);
     csx730_creat(home_root, true);
     csx730_creat(home_root_readme, false);
+    csx730_creat(home_root_readme2, false);
 
     // open and write to the file
     fd_t fd = csx730_open(home_root_readme);
     const char *msg = "stuff";
     csx730_write(fd, (void *)msg, strlen(msg));
+
+    const char *msg2 = "thing";
+    fd_t fd3 = csx730_open(home_root_readme2);
+    csx730_write(fd3, (void *)msg2, strlen(msg));
 
     // read from the file
     char buffer[256];
@@ -38,6 +44,12 @@ int main()
     csx730_read(fd, buffer, 5);
     buffer[5] = '\0';
     printf("%s\n", buffer);
+
+    char buffer2[256];
+    csx730_seek(fd3, 0);
+    csx730_read(fd3, buffer2, 6);
+    buffer2[5] = '\0';
+    printf("%s\n", buffer2);
 
     // 2
 
